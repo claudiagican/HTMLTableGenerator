0f58ace2a3e521aa8e08d4ed15299bb0a92a5580
Claudia Gican <meetsunro@gmail.com>
2017-04-30 13:04:35 +0200

    Initial commit

---
+++ HTMLCodeGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLTableGenerator
+{
+    public class HTMLCodeBuilder
+    {
+        public string SourceFolder { get; set; }
+        public string Template { get; internal set; }
+        public int ImageWidth { get; set; }
+        public int ColumnsNumber { get; set; }
+        public string ImageURLRoot { get; set; }
+        public bool UsingTable { get; set; }
+        public string ErrorMessages { get; set; }
+
+        // isTable
+        // ColumnsNumber
+
+        internal string Builder()
+        {
+            string result = "";
+            string[] allFiles = null;
+
+            try
+            {
+                allFiles = Directory.GetFiles(SourceFolder);
+
+            } catch (Exception e)
+            {
+                result = "Source folder not valid";
+                return result;
+            }
+
+            if (UsingTable)
+            {
+                result += "<table style=\"table-layout: fixed;\"><colgroup><col width=\"50%\"></col><col width=\"50%\"></col></colgroup><tbody>";
+                result += "\n<tr>\n";
+            }
+
+            int count = 0;
+            int columnIndex = 1;
+
+            for (int i = 0; i < allFiles.Length; i++)
+            {
+                count++;
+                if (columnIndex % ColumnsNumber == 1)
+                {
+                    result += "\n</tr>\n<tr>\n";
+                }
+                columnIndex++;
+
+                string currentTDCell = Template;
+                string imgPath = allFiles[i];
+                string imgNameExt = imgPath.Substring(imgPath.LastIndexOf("\\") + 1);
+                string imgName = imgNameExt.Substring(0, imgNameExt.LastIndexOf("."));
+
+                currentTDCell = currentTDCell.Replace("__IMG_NAME__", imgName);
+                currentTDCell = currentTDCell.Replace("__SIZE__", ImageWidth + "");
+                currentTDCell = currentTDCell.Replace("__LINK__", ImageURLRoot + imgNameExt);
+                currentTDCell = currentTDCell.Replace("__COUNT__", count + "");
+
+                result += currentTDCell;
+
+            }
+
+            if (UsingTable)
+            { 
+                result += "\n</tbody></table>";
+            }
+            return result;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HTMLTableGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+
+            FolderPath.Text = dialog.SelectedPath;
+        }
+
+        private void CreateHTML_Click(object sender, RoutedEventArgs e)
+        {
+            HTMLCodeBuilder codeBuilder = new HTMLCodeBuilder();
+
+            codeBuilder.SourceFolder = FolderPath.Text;
+            codeBuilder.Template = HTMLTemplate.Text;
+            codeBuilder.ImageURLRoot = ImageURLRoot.Text;
+            codeBuilder.UsingTable = (bool) UsingTable.IsChecked;
+
+            try
+            {
+                codeBuilder.ColumnsNumber = Int32.Parse(ColumnsNumber.Text);
+                codeBuilder.ImageWidth = Int32.Parse(ImageWidth.Text);
+            } catch (FormatException fe)
+            {
+                System.Windows.MessageBox.Show("Error: " + fe.Message);
+            }            
+
+            Result.Text = codeBuilder.Builder();
+        }
+    }
+}
